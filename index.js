e3c8a97387819df8f15cdb946aaecef428377fff
Admin <hanhngao@gmail.com>
2023-02-13 12:33:04 +0700

    v1

--- index.js
+++ index.js
@@ -9,20 +9,25 @@ loại bỏ do trùng sẽ thay thế bằng các ký tự “*”
 
  */
 
-let array = [4, 5, 1, 0, 0, 1, 1, 1, 2, 2, 3, 3];
-const value = array
+let array = [4, 5, 1, 0, 0, 1, 1, 1, 2, 2, 3, 3]; // khai báo 1 mảng bất kỳ
+
+const newArray = array
   .filter((item, index) => array.indexOf(item) === index)
-  .sort();
+  .sort(); // sư dung filter để lấy ra các phần tử trong mảng và các phần tử là không trùng nhau => dùng sort() để sắp xếp các phần tử theo thứ tự tăng dần
+
 const NumberDuplicates = array.reduce(function (acc, el, i, arr) {
   if (arr.indexOf(el) !== i && acc.indexOf(el) < 0) {
     acc.push(el);
   }
   return acc;
 }, []);
+
 for (const item of NumberDuplicates) {
-  value.push("*");
+  newArray.push("*");
 }
-console.log(value);
+// su dụng for lặp qua mảng chứa các phàn tử đã xuát hiện nhiều hơn 1 lần và push (*) vào
+
+console.log(newArray);
 
 /* Bài 2 : Cho 1 mảng số tự nhiên ngẫu nhiên không rỗng, trong đó các số xuất hiện nhiều lần trừ
  1 số duy nhật xuất hiện 1 lần. Viết thuật toán để hiển thị số duy nhất trong mảng cho trước. 
@@ -74,7 +79,7 @@ console.log(Math.max(sumNumberEven, sumNumberOdd));
  • Giá trị mỗi phần tử từ -105 đến 105
 */
 
-var threeSum = function (nums) {
+const threeSum = function (nums) {
   let result = [];
   if (nums.length < 3) return result;
   nums.sort((a, b) => a - b);
@@ -86,16 +91,16 @@ var threeSum = function (nums) {
 
     while (j < k) {
       let sum = nums[i] + nums[j] + nums[k];
-      if ((sum = 0)) {
+      if (sum > 0) {
+        k--;
+      } else if (sum < 0) {
+        j++;
+      } else {
         result.push([nums[i], nums[j], nums[k]]);
         while (nums[j] == nums[j + 1]) j++;
         while (nums[k] == nums[k - 1]) k--;
         k--;
         j++;
-      } else if (sum < 0) {
-        j++;
-      } else {
-        k--;
       }
     }
   }
